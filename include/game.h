ca69572b67d230e67a5d70264d8fcd34270d95bf
santos-lucasm <santos.lucasmmatheus@gmail.com>
2023-06-18 13:45:38 -0300

    move old ev_handler to Game class
    - SDL cannot handle render and event polling in different threads

--- include/game.h
+++ include/game.h
@@ -2,6 +2,7 @@
 #define __GAME_H__
 
 #include <memory>
+#include <atomic>
 #include "window.h"
 #include "world.h"
 
@@ -12,9 +13,13 @@ public:
     void Run();
 
     std::shared_ptr<Window> CurrentWindow();
+
+    bool IsRunning();
 private:
     std::shared_ptr<Window> window_;
     std::unique_ptr<World> world_;
+    bool is_running_; //TODO: change this to atomic
+    SDL_Event event_;
 };
 
 #endif
--- src/game.cpp
+++ src/game.cpp
@@ -1,32 +1,47 @@
 #include <thread>
 #include "game.h"
-#include "ev_handler.h"
 //-----------------------------------------------------------------------------
-constexpr unsigned int W_WIDTH = 1024;
-constexpr unsigned int W_HEIGHT = 768;
+constexpr unsigned int W_WIDTH = 512;//1024;
+constexpr unsigned int W_HEIGHT = 384;//768;
 //-----------------------------------------------------------------------------
-Game::Game()
+Game::Game() : is_running_(false)
 {
     window_ = std::make_shared<Window>(W_WIDTH, W_HEIGHT);
-    world_  = std::make_unique<World>(W_WIDTH/16, W_HEIGHT/16);
-
-    world_->ProceduralGeneration();
-    world_->Draw();
+    // world_  = std::make_unique<World>(W_WIDTH/16, W_HEIGHT/16);
+    // world_->ProceduralGeneration();
+    // world_->Draw();
 }
 //-----------------------------------------------------------------------------
 void Game::Run()
 {
-    //TODO: create threads vector when needed
-    auto ev_handler = std::make_unique<EventHandler>(this);
-    std::thread game_t(&EventHandler::Run, ev_handler.get());
+    is_running_ = true;
 
-    while(ev_handler->IsRunning());
-    game_t.join();
-    std::cout << "game ended" << std::endl;
+    while(IsRunning())
+    {
+        while (SDL_PollEvent(&event_))
+        {
+            switch(event_.type)
+            {
+                case SDL_QUIT:
+                    is_running_ = false;
+                    break;
+                default:
+                    std::cout << "w title: " << CurrentWindow().get()->GetTitle() << std::endl;
+                    break;
+            }
+        }
+
+    }
+    // window_->Run();
 }
 //-----------------------------------------------------------------------------
 std::shared_ptr<Window> Game::CurrentWindow()
 {
     return window_;
+}
+//-----------------------------------------------------------------------------
+bool Game::IsRunning()
+{
+    return is_running_;
 }
 //-----------------------------------------------------------------------------
