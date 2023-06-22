8518ccf51555de29636170c51b87d7024c081b0d
santos-lucasm <santos.lucasmmatheus@gmail.com>
2023-06-21 23:07:27 -0300

    add sdl2 image cmake config to install dependencies

---
+++ .github/workflows/sdl2_image-config.cmake
@@ -0,0 +1,7 @@
+set(prefix "/usr") 
+set(exec_prefix "${prefix}")
+set(libdir "${prefix}/lib/x86_64-linux-gnu")
+set(SDL2_IMAGE_PREFIX "/usr")
+set(SDL2_IMAGE_EXEC_PREFIX "/usr")
+set(SDL2_IMAGE_INCLUDE_DIRS "${prefix}/include/SDL2_image")
+set(SDL2_IMAGE_LIBRARIES "-lSDL2_image")
